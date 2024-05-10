f290d3c3502656fb683e8ba330af5fb07d9a2f31
Tiago Amaral Ferreira <82990234+TiagoAmaralFerreira@users.noreply.github.com>
2024-05-10 16:02:31 -0300

    feat: Criação do pacote de response, para retornar os dados ou o erro ao tentar inserir no banco.

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -4,33 +4,46 @@ import (
 	"api/src/db"
 	"api/src/models"
 	"api/src/repositories"
+	"api/src/response"
 	"encoding/json"
 	"io/ioutil"
-	"log"
 	"net/http"
 )
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 	bodyRequest, erro := ioutil.ReadAll(r.Body)
 	if erro != nil {
-		log.Fatal(erro)
+		response.Erro(w, http.StatusUnprocessableEntity, erro)
 		return
 	}
 
 	var user models.User
 	if erro := json.Unmarshal(bodyRequest, &user); erro != nil {
-		log.Fatal(erro)
+		response.Erro(w, http.StatusBadRequest, erro)
+		return
 	}
 
 	db, erro := db.Connect()
 
 	if erro != nil {
-		log.Fatal(erro)
+		response.Erro(w, http.StatusInternalServerError, erro)
 		return
 	}
 
+	defer db.Close()
+
 	repository := repositories.NewRepositoriesUsers(db)
-	repository.Create(user)
+
+	userId, erro := repository.Create(user)
+
+	if erro != nil {
+		response.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	user.ID = userId
+	response.JSON(w, http.StatusCreated, user)
+
 }
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
--- src/repositories/users.go
+++ src/repositories/users.go
@@ -13,6 +13,25 @@ func NewRepositoriesUsers(db *sql.DB) *Users {
 	return &Users{db}
 }
 
-func (u Users) Create(user models.User) (uint64, error) {
-	return 0, nil
+func (repositoryUser Users) Create(user models.User) (uint64, error) {
+	statement, erro := repositoryUser.db.Prepare("insert into users (name, nick, email, password) values(?, ?, ?, ?)")
+	if erro != nil {
+		return 0, erro
+	}
+	defer statement.Close()
+
+	result, erro := statement.Exec(user.Name, user.Nick, user.Email, user.Password)
+
+	if erro != nil {
+		return 0, erro
+	}
+
+	lastInsertId, erro := result.LastInsertId()
+
+	if erro != nil {
+		return 0, erro
+	}
+
+	return uint64(lastInsertId), nil
+
 }
