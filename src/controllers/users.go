8a7b95dfe10c3da8e1e0d29bd8032c9f6fb2f7d7
Tiago Amaral Ferreira <82990234+TiagoAmaralFerreira@users.noreply.github.com>
2024-05-10 15:01:36 -0300

    feat: Criação do repositório para auxiliar o controller a salvar os dados no banco.

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -1,9 +1,36 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/db"
+	"api/src/models"
+	"api/src/repositories"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando Usuário!"))
+	bodyRequest, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		log.Fatal(erro)
+		return
+	}
+
+	var user models.User
+	if erro := json.Unmarshal(bodyRequest, &user); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := db.Connect()
+
+	if erro != nil {
+		log.Fatal(erro)
+		return
+	}
+
+	repository := repositories.NewRepositoriesUsers(db)
+	repository.Create(user)
 }
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
--- src/router/routes/routes.go
+++ src/router/routes/routes.go
@@ -1,11 +1,26 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
 
-//Routes representa todas as rotas da API
+	"github.com/gorilla/mux"
+)
+
+// Routes representa todas as rotas da API
 type Routes struct {
 	Uri                string
 	Metodo             string
 	Funcao             func(http.ResponseWriter, *http.Request)
 	RequerAutenticacao bool
+}
+
+// Configurar todas as rotas dentro do Router
+func ConfigRoutes(r *mux.Router) *mux.Router {
+	routes := routesUsers
+
+	for _, route := range routes {
+		r.HandleFunc(route.Uri, route.Funcao).Methods(route.Metodo)
+	}
+
+	return r
 }
