2c9835d725f45691714dc45fa697b62f5e83d37b
Tiago Amaral Ferreira <82990234+TiagoAmaralFerreira@users.noreply.github.com>
2024-05-22 13:16:51 -0300

    fix: Ajuste wd

--- src/config/config.go
+++ src/config/config.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"runtime"
 	"strconv"
 
 	"github.com/joho/godotenv"
+)
+
+const (
+	LOCAL = "/.env"
 )
 
 var (
@@ -19,27 +22,23 @@ var (
 func Load() error {
 	var erro error
 
-	if erro = godotenv.Load(); erro != nil {
+	wd, _ := os.Getwd()
+
+	if erro = godotenv.Load(wd + LOCAL); erro != nil {
 		log.Fatal(erro)
 	}
 
-	port := fmt.Sprintf(os.Getenv("API_PORT"))
-	user := fmt.Sprintf(os.Getenv("DB_USER"))
-	pass := fmt.Sprintf(os.Getenv("DB_PASSWORD"))
-	name := fmt.Sprintf(os.Getenv("DB_NAME"))
-	host := fmt.Sprintf(os.Getenv("DB_HOST"))
+	port := fmt.Sprintf(wd + os.Getenv("API_PORT"))
+	user := fmt.Sprintf(wd + os.Getenv("DB_USER"))
+	pass := fmt.Sprintf(wd + os.Getenv("DB_PASSWORD"))
+	name := fmt.Sprintf(wd + os.Getenv("DB_NAME"))
+	host := fmt.Sprintf(wd + os.Getenv("DB_HOST"))
 
 	Porta, erro = strconv.Atoi(port)
 	if erro != nil {
 		Porta = 5000
 	}
-
-	var wd string
-	if runtime.GOOS != "windows" {
-		wd, _ = os.Getwd()
-	}
-
-	// Construir a string de conexão corretamente
+	// postgres://%s:%s@%s/devbook
 	StringConexaoBanco = fmt.Sprintf("postgres://%s:%s@%s/%s",
 		user,
 		pass,
@@ -47,10 +46,5 @@ func Load() error {
 		name,
 	)
 
-	if wd != "" {
-		StringConexaoBanco = fmt.Sprintf("%s/%s", wd, StringConexaoBanco)
-	}
-
 	return nil
-
 }
